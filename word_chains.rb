da9385ca329ca0526337271630b81ffb1a1b42dd
Tripp Davis <trippdavis3@gmail.com>
2015-02-23 21:53:10 -0800

    Stop adjacent word search if target is found and trace path from target to source. Also included message if chain is not possible.

--- word_chains.rb
+++ word_chains.rb
@@ -2,29 +2,48 @@ require 'set'
 
 class WordChainer
   attr_reader :dictionary
-  attr_accessor :current_words, :all_seen_words
+  attr_accessor :current_words, :all_seen_words, :word_found
 
   def initialize(dictionary_file)
     @dictionary = (File.readlines(dictionary_file).map(&:chomp)).to_set
   end
 
   def run(source, target)
+    @word_found =  false
     @current_words = [source]
     @all_seen_words = { source => nil }
     until current_words.empty?
       current_word = current_words.shift
-      new_adjacent_words(current_word)
+      new_adjacent_words(current_word, target)
+    end
+
+    if !word_found
+      "No chain detected."
+    else
+      build_path(target)
     end
   end
 
-  def new_adjacent_words(current_word)
+  def build_path(target)
+    return [target] if all_seen_words[target].nil?
+    prev_word = all_seen_words[target]
+    build_path(prev_word) + [target]
+  end
+
+
+  def new_adjacent_words(current_word, target)
     new_words = adjacent_words(current_word)
     new_words.each do |new_word|
       unless all_seen_words.include?(new_word)
         all_seen_words[new_word] = current_word
+        if new_word == target
+          @word_found = true
+          return
+        end
         current_words << new_word
       end
     end
+
   end
 
   def adjacent_words(word)
